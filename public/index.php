b393718c625c00f05a555d558a9f61ea7700368b
Joey Rivera <joey.rivera@cengage.com>
2011-04-29 09:55:45 -0400

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+define('APPLICATION_ENV', 'development');
+define('ZF_LIBRARY', 'C:\\Documents and Settings\\jorivera\\My Documents\\htdocs\\library\\zend\\1.11.4');
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+	realpath(ZF_LIBRARY),
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap()
+            ->run();
